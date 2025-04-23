840d04458bdc4596d9fdba4d32f4dfa6c272ff82
Daniel Archer <daniel.archer.dev@outlook.com>
2025-04-23 09:27:58 -0300

    work in progress on demo

--- risk_calculator/demo.py
+++ risk_calculator/demo.py
@@ -4,14 +4,10 @@ import csv
 # from configparser import ConfigParser
 import configparser
 import json_to_csv
-import pandas
+import pandas as pd
+from io import StringIO
 
-def load_secrets_conf():
-    if os.path.exists('secrets.conf'):
-        print("has file")
-
-def main():
-    # args = parser.parse_args()
+def get_client():
     config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
     config.read('./secrets/secrets.conf')
 
@@ -20,32 +16,54 @@ def main():
     callback_url = config['AppSecrets']['callback_url']
     token_path = config['AppSecrets']['token_path']
 
-    c = auth.easy_client(api_key, app_secret, callback_url, token_path)
+    return auth.easy_client(api_key, app_secret, callback_url, token_path)
 
+def get_config():
+    config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
+    config.read('./secrets/local_config.conf')
+    return config
+
+def main():
+    # args = parser.parse_args()
+    c = get_client()
+    conf = get_config()
     # r = c.get_price_history_every_day('AAPL')
     # r.raise_for_status()
     # print(json.dumps(r.json(), indent=4))
 
-    r2 = c.get_instruments('AAPL',c.Instrument.Projection.FUNDAMENTAL)
+    # r2 = c.get_instruments('AAPL,MSFT,GME,NTB',c.Instrument.Projection.FUNDAMENTAL)
+    r2 = c.get_instrument('MSFT',c.Instrument.Projection.FUNDAMENTAL)
     r2.raise_for_status()
     # print(json.dumps(r2.json(), indent=4))
     
-    # output_file = './data/output.json'
-    # with open(output_file, 'w') as json_file:
-    #     json.dump(r2.json(), json_file)
+    in_memory_file = StringIO()
 
-    output_csv = './data/output.csv'
-    with open(output_csv, "w", newline="") as csv_file:
-        csv_writer = csv.writer(csv_file)  
-        # Write the header
-        data = json.loads(r2.json())
-        print(data['instruments'])
-        # header = data[0].keys()
-        # csv_writer.writerow(header)
+    # json.dump(r2.json(), in_memory_file)
+    # in_memory_file.write(json.dumps(r2.json()))
+    # in_memory_file.write("hello")
+    df = pd.read_json(json.dumps(r2.json()))
+    # print(in_memory_file.readlines)
+    # df = pd.read_json(in_memory_file)
+    print(df)
 
-        # # Write the data rows
-        # for row in data:
-        #     csv_writer.writerow(row.values())
+
+    output_file = conf['AppConfig']['output_file']
+    with open(output_file, 'w') as json_file:
+        json.dump(r2.json(), json_file)
+
+
+    # output_csv = './data/output.csv'
+    # with open(output_csv, "w", newline="") as csv_file:
+    #     csv_writer = csv.writer(csv_file)  
+    #     # Write the header
+    #     data = json.loads(r2.json())
+    #     print(data['instruments'])
+    #     # header = data[0].keys()
+    #     # csv_writer.writerow(header)
+
+    #     # # Write the data rows
+    #     # for row in data:
+    #     #     csv_writer.writerow(row.values())
 
 if __name__ == '__main__':
     # if RUN_ARGS.getboolean('profile'):

18eb3bd74b20a289825fd8287431d7492fc096a7
Daniel Archer <daniel.archer.dev@outlook.com>
2025-05-02 14:51:16 -0300

    what I needed was list comprehension

--- risk_calculator/accounts/securities_account.py
+++ risk_calculator/accounts/securities_account.py
@@ -5,29 +5,20 @@ import accounts.initial_balances as initial_balances
 import accounts.position as pos
 
 class SecuritiesAccount():
+
     def __init__(self, dataframe):
     # self.parse_args()
     # self.read_config()
     # self.get_client()
-        self.securitiesAccount = dataframe['securitiesAccount']
+        self.securitiesAccount = dataframe
         self.AccountType = self.securitiesAccount['type'] 
         self.AccountNumber = self.securitiesAccount['accountNumber']
         self.RoundTrips = self.securitiesAccount['roundTrips']
         self.isDayTrader = self.securitiesAccount['isDayTrader']
         self.isClosingOnlyRestricted = self.securitiesAccount['isClosingOnlyRestricted']
         self.pfcbFlag = self.securitiesAccount['pfcbFlag']
-        self.Positions = self.parse_positions(self.securitiesAccount['positions'])
-                
+
+        self.Positions = [pos.Position(position) for position in self.securitiesAccount['positions']]
+
         self.CurrentBalances = current_balances.CurrentBalances(self.securitiesAccount['currentBalances'])
         self.InitialBalances = initial_balances.InitialBalances(self.securitiesAccount['initialBalances'])
-
-
-    def parse_positions(self, pos_data):
-        Positions = []
-        for po in range(len(pos_data)):
-            # print(po)
-            position = pos.Position(pos_data[po])
-            Positions.append(position)
-        return Positions
-
-

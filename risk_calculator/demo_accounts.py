2d999d1cc60f715fb50f22bbae40254ae1b564f5
Daniel Archer <daniel.archer.dev@outlook.com>
2025-05-16 14:27:45 -0300

    clean up demo script main body

--- risk_calculator/demo_accounts.py
+++ risk_calculator/demo_accounts.py
@@ -5,11 +5,14 @@ import accounts.accounts as accounts
 import datetime
 import time
 import schwab
+import cufflinks
 
 def main():
     #these are for live:
     acct = accounts.AccountsLauncher()
     acct.run()
+
+    client = acct.client
 
     #these are for file
     # securities_account_file = './data/output_2025-05-02.json'
@@ -23,58 +26,15 @@ def main():
 
 
     # high level reporting
-    lines = []
-    print(str.format("My net liquidation value is: {0}", securities_account.CurrentBalances.LiquidationValue))
-    print(str.format("My total cash is: {0}", securities_account.CurrentBalances.CashBalance))
+    print_welcome(securities_account)
+    # acct.client.get
+    r2 = acct.client.get_instruments('MSFT',acct.client.Instrument.Projection.FUNDAMENTAL)
+    response = json.dumps(r2.json())
+    print(response)
 
-    print("My stock positions are:")
-    for pos in securities_account.Positions:
-        # print(pos)
-        if(pos.instrument.AssetType == 'EQUITY'):
-            line = str.format("{0},{1},{2}", pos.symbol, pos.Quantity, pos.marketValue)
-            print(line)
+    price_history = client.get_price_history_every_minute('MSFT')
 
-    print("My options are:")
-    for pos in securities_account.Positions:
-        # print(pos)
-        if(pos.instrument.AssetType == 'OPTION'):
-            line = str.format("{0},{1},{2}", pos.instrument.description, pos.Quantity, pos.marketValue)
-            print(line)
-
-    # print(acct.Transactions)
-
-    print("My transactions are:")
-    for tran in transaction.Transactions:
-        default_transaction_history_lookback = int(acct.config['AppConfig']['default_transaction_history_lookback'])
-        earliest_date = datetime.datetime.now() - datetime.timedelta(days=default_transaction_history_lookback)
-        
-        tradeDate = datetime.datetime.fromisoformat(tran.tradeDate)
-        # date
-        if  tradeDate.date() > earliest_date.date():
-
-            if tran.type == 'TRADE':
-                    
-                # print(tradeDate.strftime('%Y-%m-%d %H:%M'))
-                # line = str.format("{0},{1},{2},{3},{4}", tradeDate.strftime('%Y-%m-%d %H:%M'), tran.activityId, tran.description, tran.type, tran.netAmount)
-                line = str.format("{0},{1},{2},{3},{4},{5}", tradeDate.date(), tradeDate.time(), tran.type, tran.orderId, tran.description,tran.tradeDate)
-            
-                print(line)
-                for transferItem in tran.transferItems:
-                    # if(transferItem.
-                    if(transferItem.assetType == 'CURRENCY'):
-                        line = transferItem.feeType, transferItem.amount, transferItem.cost
-                    # line = str.format("{0},{1},{2},{3},{4}", transferItem.assetType,transferItem.symbol,transferItem.description,transferItem.amount,transferItem.feeType)
-                        print(line)
-                    effect = ''
-                    try:
-                        effect = transferItem.positionEffect
-                        print(effect)
-                    except:
-                        print("no effect")
-            else:
-                print(tran)
-
-    
+    print(json.dumps(price_history.json()))
 
 def market_hours():
     # resp = launcher.client.get_transactions(launcher.hash)
@@ -85,7 +45,56 @@ def market_hours():
 def load_account_file(securities_account_file, transactions_file):
     return accounts.AccountsLauncher(securities_account_file=securities_account_file, transactions_file=transactions_file)
 
+def print_welcome(securities_account):
+        print(str.format("My net liquidation value is: {0}", securities_account.CurrentBalances.LiquidationValue))
+        print(str.format("My total cash is: {0}", securities_account.CurrentBalances.CashBalance))
 
+        print("My stock positions are:")
+        for pos in securities_account.Positions:
+            # print(pos)
+            if(pos.instrument.AssetType == 'EQUITY'):
+                line = str.format("{0},{1},{2}", pos.symbol, pos.Quantity, pos.marketValue)
+                print(line)
+
+def print_options(securities_account):
+        print("My options are:")
+        for pos in securities_account.Positions:
+            # print(pos)
+            if(pos.instrument.AssetType == 'OPTION'):
+                line = str.format("{0},{1},{2}", pos.instrument.description, pos.Quantity, pos.marketValue)
+                print(line)
+
+def print_transactions(transaction):
+        print("My transactions are:")
+        for tran in transaction.Transactions:
+            default_transaction_history_lookback = int(acct.config['AppConfig']['default_transaction_history_lookback'])
+            earliest_date = datetime.datetime.now() - datetime.timedelta(days=default_transaction_history_lookback)
+            
+            tradeDate = datetime.datetime.fromisoformat(tran.tradeDate)
+            # date
+            if  tradeDate.date() > earliest_date.date():
+
+                if tran.type == 'TRADE':
+                        
+                    # print(tradeDate.strftime('%Y-%m-%d %H:%M'))
+                    # line = str.format("{0},{1},{2},{3},{4}", tradeDate.strftime('%Y-%m-%d %H:%M'), tran.activityId, tran.description, tran.type, tran.netAmount)
+                    line = str.format("{0},{1},{2},{3},{4},{5}", tradeDate.date(), tradeDate.time(), tran.type, tran.orderId, tran.description,tran.tradeDate)
+                
+                    print(line)
+                    for transferItem in tran.transferItems:
+                        # if(transferItem.
+                        if(transferItem.assetType == 'CURRENCY'):
+                            line = transferItem.feeType, transferItem.amount, transferItem.cost
+                        # line = str.format("{0},{1},{2},{3},{4}", transferItem.assetType,transferItem.symbol,transferItem.description,transferItem.amount,transferItem.feeType)
+                            print(line)
+                        effect = ''
+                        try:
+                            effect = transferItem.positionEffect
+                            print(effect)
+                        except:
+                            print("no effect")
+                else:
+                    print(tran)
 
 if __name__ == '__main__':
     # if RUN_ARGS.getboolean('profile'):

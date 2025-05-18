d4e8c3faabd700e5c7d16785f69160fb302a7b08
Daniel Archer <daniel.archer.dev@outlook.com>
2025-05-18 14:55:28 -0300

    first demo on charts

--- risk_calculator/demo_accounts.py
+++ risk_calculator/demo_accounts.py
@@ -28,44 +28,75 @@ def main():
     securities_account = acct.SecuritiesAccount
     transaction = acct.Transactions
 
-    
-
     # high level reporting
     print_welcome(securities_account)
 
-    earliest_date = datetime.datetime.now() - datetime.timedelta(days=1)
-            
+    # print_15_mins('NTB', client)
+
+    # print_15_mins('GME', client)
 
     # price_history = client.get_price_history_every_minute('MSFT', start_datetime=earliest_date).json()
-    price_history = client.get_price_history_every_fifteen_minutes('MSFT', start_datetime=earliest_date).json()
 
-    # daily 
+    print_180_daily('GME', client)
 
+
+
+def print_15_mins(symbol, client):
+    earliest_date = datetime.datetime.now() - datetime.timedelta(days=1)
+    price_history = client.get_price_history_every_fifteen_minutes(symbol, start_datetime=earliest_date).json()
+
+    df = price_history_to_dataframe(price_history)
+
+    my_plot_settings(symbol, df)
+
+def print_180_daily(symbol, client):
+    earliest_date = datetime.datetime.now() - datetime.timedelta(days=180)
+    price_history = client.get_price_history_every_day(symbol, start_datetime=earliest_date).json()
+
+    df = price_history_to_dataframe(price_history)
+
+    my_plot_settings(symbol, df)
+
+
+def price_history_to_dataframe(price_history):
     df = pd.DataFrame(price_history['candles'], columns=['open', 'high', 'low', 'close', 'volume', 'datetime'])
     df['datetime'] = df['datetime'].apply(date_transform)
     df = df.set_index('datetime')
     # dt_range = pd.date_range(start="2025-05-01", end="2025-05-16")
     # df = df[df.index > (dt_range)]
     # df = df[df.index.]
-    
+    return df
 
-    #this example does not work
-                # candlestick = go.Candlestick(x=df.index,
-                #                              open=df['open'],
-                #                              high=df['high'],
-                #                              low=df['low'],
-                #                              close=df['close'])
-                
-                # fig = go.Figure(data=[candlestick])
-                # fig.show()
-
+def my_plot_settings(symbol, df):
     fplt.plot(
-            df,
-            type='candle',
-            style='charles',
-            title='MSFT, May - 2025',
-            ylabel='Price ($)'
-        )
+    df,
+    type='candle',
+    style='charles',
+    title='{0}, May - 2025'.format(symbol),
+    ylabel='Price ($)',
+    figsize=(21,6),
+    savefig=dict(fname='{0}_chart.png'.format(symbol), dpi=1200)
+    )
+
+    # fig = fplt.figure(figsize=(21, 7))  # Wider chart
+    # ax = fig.add_subplot(1, 1, 1)
+
+    # fplt.plot(df,
+    #           type='candle',
+    #           style='charles',
+    #         #   suptitle='{0}, May - 2025'.format(symbol),
+    #           ylabel='Price ($)',
+    #           ax=ax,
+    #           savefig=dict(fname='{0}_chart.png'.format(symbol), dpi=1200))
+    # fig.show()
+
+
+    # fig, ax = fplt.plot(df, type='candle', style='charles', returnfig=True)
+    # fig.update_layout(width=1200, height=600)
+    # fig.savefig('{0}_chart.png'.format(symbol), dpi=300)
+    # fig.show()
+
+    
 
 
 

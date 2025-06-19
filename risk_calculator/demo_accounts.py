759fbb21341c1f2ea96346240cfbca2c3f1ba341
Daniel Archer <daniel.archer.dev@outlook.com>
2025-06-19 10:39:34 -0300

    demo to print daily report

--- risk_calculator/demo_accounts.py
+++ risk_calculator/demo_accounts.py
@@ -15,17 +15,35 @@ lines = []
 print(str.format("My net liquidation value is: {0}", acct.SecuritiesAccount.CurrentBalances.LiquidationValue))
 print(str.format("My total cash is: {0}", acct.SecuritiesAccount.CurrentBalances.CashBalance))
 
-print("My positions are:")
+print("My stock positions are:")
 for pos in acct.SecuritiesAccount.Positions:
     # print(pos)
-    print(str.format("{0},{1},{2}", pos.symbol, pos.Quantity, pos.marketValue))
+    if(pos.instrument.AssetType == 'EQUITY'):
+        line = str.format("{0},{1},{2}", pos.symbol, pos.Quantity, pos.marketValue)
+        print(line)
 
-print(acct.Transactions)
-# resp = launcher.client.get_transactions(launcher.hash)
-# resp = launcher.client.get_market_hours(markets=schwab.client.Client.MarketHours.Market.OPTION)
-# print(resp.json())
+print("My options are:")
+for pos in acct.SecuritiesAccount.Positions:
+    # print(pos)
+    if(pos.instrument.AssetType == 'OPTION'):
+        line = str.format("{0},{1},{2}", pos.instrument.description, pos.Quantity, pos.marketValue)
+        print(line)
 
-# # look at previous files
-# input_file = './data/output.json'
-# older = accounts.AccountsLauncher(input_file)
-# print(older.Account.CurrentBalances.CashBalance)
+# print(acct.Transactions)
+
+
+
+
+
+def market_hours():
+    # resp = launcher.client.get_transactions(launcher.hash)
+    # resp = launcher.client.get_market_hours(markets=schwab.client.Client.MarketHours.Market.OPTION)
+    # print(resp.json())
+    return None
+
+def work_with_old_file():
+    # # look at previous files
+    # input_file = './data/output.json'
+    # older = accounts.AccountsLauncher(input_file)
+    # print(older.Account.CurrentBalances.CashBalance)
+    return None

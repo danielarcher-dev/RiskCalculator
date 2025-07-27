4dd9626d773fde38344de52cf8af6ac82825b201
Daniel Archer <daniel.archer.dev@outlook.com>
2025-07-27 14:41:47 -0300

    add annualized max return metric on options

--- risk_calculator/demo_accounts.py
+++ risk_calculator/demo_accounts.py
@@ -12,7 +12,7 @@ import pandas as pd
 import numpy as np
 from io import StringIO
 import xlsxwriter
-from datetime import datetime
+from datetime import datetime, timedelta
 import re
 import httpx
 
@@ -41,10 +41,6 @@ def main():
 
 
 
-    # Client.Client.Options.Type.
-
-
-
 
     #chart options
     print("My options are:")
@@ -53,44 +49,8 @@ def main():
         if(pos.instrument.AssetType == 'OPTION'):
 
             position_option_chain(acct, pos)
-            # chain = client.get_option_chain(
-            #     symbol=pos.instrument.underlyingSymbol,
-            #     contract_type=pos.instrument.putCall,
-            #     strategy=client.Options.Strategy.SINGLE,
-            #     strike=pos.instrument.strike,
-            #     include_underlying_quote=True,
-            #     from_date=pos.instrument.expiration,
-            #     to_date=pos.instrument.expiration
-            # )
-            
-            # assert chain.status_code == httpx.codes.OK
-            # chain_json = chain.json()
-            # options_chain_name = acct.options_chain_file.replace("<symbol>", pos.symbol)
-            # with open(options_chain_name, 'w') as json_file:
-            #     json.dump(chain_json, json_file)
-            
-            # for dte in chain_json["putExpDateMap"]:
-            #     if str(pos.instrument.expiration) == dte.split(":")[0]:
-            #         for strike in chain_json["putExpDateMap"][dte]:
-            #             if str(pos.instrument.strike) == strike:
-            #                 # If I've nested this correctly, there should only be one result matching dte and strike
-            #                 chain_values = chain_json["putExpDateMap"][dte][strike][0]
-
-            #                 premium_bid = chain_values["bid"]
-            #                 daysToExpiration = chain_values["daysToExpiration"]
-            # # premium_bid = chain_json["putExpDateMap"].values()[0].values()[0]["bid"]
-            # strike_price = pos.instrument.strike
-            # q_ratio = round((premium_bid / (strike_price * 100)) * (365 / daysToExpiration), 4)
-
-            # line = str.format("{0},{1},{2},{3},{4}", pos.instrument.underlyingSymbol, pos.instrument.description, pos.Quantity, pos.marketValue, q_ratio)
-            # print(line)
-            # print(premium_bid, strike_price, daysToExpiration)            
-
-
-
-
-
-
+        
+    get_option_chain(acct, "MSFT", SchwabClient.Client.Options.ContractType.PUT )
     
     # print_my_watchlist(watchlist_file=acct.watchlist)
 
@@ -200,70 +160,119 @@ def position_option_chain(acct, pos):
 
     get_option_chain(acct, underlying_symbol, contract_type, strike, expiration, strategy, include_underlying_quote)
 
-def get_option_chain(acct, underlying_symbol, contract_type, strike, expiration, strategy=SchwabClient.Client.Options.Strategy.SINGLE, include_underlying_quote=True):
+def get_option_chain(acct, underlying_symbol, contract_type, strike=None, expiration=None, strategy=SchwabClient.Client.Options.Strategy.SINGLE, include_underlying_quote=True):
 
-    chain = acct.client.get_option_chain(
-        symbol=underlying_symbol,
-        contract_type=contract_type,
-        strategy=strategy,
-        strike=strike,
-        include_underlying_quote=include_underlying_quote,
-        from_date=expiration,
-        to_date=expiration
-    )
+    if expiration == None:
+        # This section allows me to re-use the main body while simplifying ad-hoc queries
+        target_date = (datetime.now() + timedelta(days=7))
+        from_date = (datetime.now())
+    else:
+         target_date = expiration
+         from_date = expiration
+
+    if strike == None:
+        chain = acct.client.get_option_chain(
+            symbol=underlying_symbol,
+            contract_type=contract_type,
+            strategy=strategy,
+            strike_count=10,
+            include_underlying_quote=include_underlying_quote,
+            from_date=from_date,
+            to_date=target_date
+        )
+    else:
+        chain = acct.client.get_option_chain(
+            symbol=underlying_symbol,
+            contract_type=contract_type,
+            strategy=strategy,
+            strike=strike,
+            include_underlying_quote=include_underlying_quote,
+            from_date=from_date,
+            to_date=target_date
+        )
     
     assert chain.status_code == httpx.codes.OK
     chain_json = chain.json()
 
+    # TODO: until I'm splitting out the individual dte/strike element, just save this at the underlyingSymbol level
+    options_chain_name = acct.options_chain_file.replace("<symbol>", underlying_symbol)
+    with open(options_chain_name, 'w') as json_file:
+        json.dump(chain_json, json_file)
 
     if contract_type == SchwabClient.Client.Options.ContractType.PUT:
         ExpDateMap = "putExpDateMap"
     elif contract_type == SchwabClient.Client.Options.ContractType.CALL:
         ExpDateMap = "callExpDateMap"
     else:
+        # TODO: currently, if we want both PUTs and CALLs, we need to order each one, or implement logic to handle ContractType.ANY
         raise Exception("invalid contract_type: {0}".format(contract_type))
     
+    found_flag = False
     for dte in chain_json[ExpDateMap]:
+        # This logic assumes options are traded for this symbol on given expiration date
         if str(expiration) == dte.split(":")[0]:
-
             for chain_strike in chain_json[ExpDateMap][dte]:
                 if strike == float(chain_strike):
                     # If I've nested this correctly, there should only be one result matching dte and strike
                     chain_values = chain_json[ExpDateMap][dte][chain_strike][0]
-                    option_symbol = chain_json["symbol"]
+                    option_symbol = chain_values["symbol"]
                     premium_bid = chain_values["bid"]
                     daysToExpiration = chain_values["daysToExpiration"]
                     description = chain_values["description"]
-    options_chain_name = acct.options_chain_file.replace("<symbol>", option_symbol)
-    with open(options_chain_name, 'w') as json_file:
-        json.dump(chain_json, json_file)
 
-    q_ratio = round((premium_bid / (strike * 100)) * (365 / daysToExpiration), 4)
+                    # TODO: add a handler for this
+                    required_capital = strike * 100
+                    premium_collected = premium_bid * 100
+                    max_return_on_risk_pct = round((premium_collected / required_capital * 100), 4)
+                    # annualized_return_on_risk_pct = max_return_on_risk_pct * 52
+                    annualized_return_on_risk_pct = round((max_return_on_risk_pct / 365 / daysToExpiration), 4)
 
-    line = str.format("{0},{1},{2}", underlying_symbol, description, q_ratio)
-    print(line)
-    print(premium_bid, strike, daysToExpiration)  
+                    q_ratio = annualized_return_on_risk_pct
 
+                    line = str.format("{0},{1},dte={2}, bid={3}, q={4}%", underlying_symbol, description, daysToExpiration, premium_bid, q_ratio)
+                    print(line)
 
-# def parse_occ_symbol(symbol):
-#     match = re.match(r'([A-Z ]{6})(\d{6})([CP])(\d{8})', symbol)
-#     if not match:
-#         return None
+                    # print("Capital = {0}".format(required_capital))
+                    # print("Premium = {0}".format(premium_collected))
+                    # print("Max profit = {0}%".format(max_return_on_risk_pct))
+                    # print("Annualized: {0}%".format(annualized_return_on_risk_pct))
+                    # print(premium_bid, strike, daysToExpiration) 
 
-#     # root = match.group(1).strip()
-#     date_str = match.group(2)
-#     option_type = match.group(3)
-#     strike_raw = match.group(4)
+                    found_flag = True
 
-#     expiration = datetime.strptime(date_str, '%y%m%d').date()
-#     strike = int(strike_raw) / 1000
+    # if we don't find the expiration and strike we're looking for, then save them all
+    if found_flag != True:
+        for dte in chain_json[ExpDateMap]:
+            # This logic assumes options are traded for this symbol on given expiration date
+            # if str(expiration) == dte.split(":")[0]:
+                for chain_strike in chain_json[ExpDateMap][dte]:
+                    # if strike == float(chain_strike):
+                        # If I've nested this correctly, there should only be one result matching dte and strike
+                        chain_values = chain_json[ExpDateMap][dte][chain_strike][0]
+                        option_symbol = chain_values["symbol"]
+                        premium_bid = chain_values["bid"]
+                        daysToExpiration = chain_values["daysToExpiration"]
+                        description = chain_values["description"]
+                        strike = chain_values["strikePrice"]
 
-#     return {
-#         # 'root': root,
-#         'expiration': expiration,
-#         'type': 'Put' if option_type == 'P' else 'Call',
-#         'strike': strike
-#     }               
+                        # TODO: add a handler for this
+                        required_capital = strike * 100
+                        premium_collected = premium_bid * 100
+                        max_return_on_risk_pct = round((premium_collected / required_capital * 100), 4)
+                        # annualized_return_on_risk_pct = max_return_on_risk_pct * 52
+                        annualized_return_on_risk_pct = round((max_return_on_risk_pct * 365 / daysToExpiration), 4)
+
+                        q_ratio = annualized_return_on_risk_pct
+
+                        line = str.format("{0},{1},dte={2}, bid={3}, q={4}%", underlying_symbol, description, daysToExpiration, premium_bid, q_ratio)
+                        print(line)
+
+                        # print("Capital = {0}".format(required_capital))
+                        # print("Premium = {0}".format(premium_collected))
+                        # print("Max profit = {0}%".format(max_return_on_risk_pct))
+                        # print("Annualized: {0}%".format(annualized_return_on_risk_pct))
+                        # print(premium_bid, strike, daysToExpiration)  
+          
 
 if __name__ == '__main__':
     # if RUN_ARGS.getboolean('profile'):
